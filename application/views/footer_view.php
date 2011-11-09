7c783948be7fab18ca574be3363e9a224e637f15
DanielMM <daniel@danielmois.com>
2011-11-09 17:53:19 +0100

    twitter feed design and favorite liks, not so sure about it

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -2,7 +2,43 @@
 		<div id="footer_wrapp">
 			<footer>
 				<article>
-					
+					<h4>Latest tweets</h4>
+					<ul id="tweets">
+						<li>
+							<h6><?php echo anchor('http://twitter.com/#!/danmois','@danmois','title="Fallow me onTwitter" target="_blank"'); ?><span>Sunday 12:34</span></h6>
+							<p>Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi susp</p>
+						</li>
+						<li>
+							<h6><?php echo anchor('http://twitter.com/#!/danmois','@danmois','title="Fallow me onTwitter" target="_blank"'); ?><span>5 hours ago</span></h6>
+							<p>Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi susp</p>
+						</li> 
+						<li>
+							<h6><?php echo anchor('http://twitter.com/#!/danmois','@danmois','title="Fallow me onTwitter" target="_blank"'); ?><span>12.10.11 16:12</span></h6>
+							<p>Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi susp</p>
+						</li>
+					</ul>
+				</article>
+				<article class="space">
+					<h4>Something</h4>
+					<ul id="favorites">
+						<li><?php echo anchor('http://net.tutsplus.com/','Nettuts+','title="Nettuts+" target="_blanck"');?></li>
+						<li><?php echo anchor('http://webdesign.tutsplus.com/','Webdesigntuts+','title="Webdesigntuts+" target="_blanck"');?></li>
+						<li><?php echo anchor('http://sixrevisions.com/','Six Revisions','title="Six Revisions" target="_blanck"');?></li>
+						<li><?php echo anchor('http://www.alistapart.com/','A List Apart','title="A List Apart" target="_blanck"');?></li>
+						<li><?php echo anchor('http://www.webresourcesdepot.com/','Web Resources Depot','title="Web Resources Depot" target="_blanck"');?></li>
+						<li><?php echo anchor('http://www.webdesignerdepot.com/','Web Designer Depot','title="Web Designer Depot" target="_blanck"');?></li>
+						<li><?php echo anchor('http://webdesignledger.com/','Web Design Ledger','title="Web Design Ledger" target="_blanck"');?></li>
+						<li><?php echo anchor('http://37signals.com/svn','37 Signals','title="37 Signals" target="_blanck"');?></li>
+						<li><?php echo anchor('http://php.net/','PHP.net','title="PHP.net" target="_blanck"');?></li>
+						<li><?php echo anchor('http://codeingniter.com','Codeigniter','title="Codeigniter" target="_blanck"');?></li>
+					</ul>
+				</article>
+				<article class="space">
+					<h4>Lorem ipsum dolor sit amet</h4>
+					<p>Lorem ipsum dolor sit amet, libero turpis non cras ligula, id commodo, aenean est in volutpat amet sodales, porttitor bibendum facilisi suspendisse, aliquam ipsum ante morbi sed ipsum mollis. Sollicitudin viverra, vel varius eget sit mollis. 
+						Commodo enim aliquam suspendisse tortor cum diam, commodo facilisis, rutrum et duis nisl porttitor, vel eleifend odio ultricies ut, orci in adipiscing felis velit nibh. Consectetuer porttitor feugiat vestibulum sit feugiat, voluptates dui eros libero. Etiam vestibulum at lectus.					
+						Donec vivamus. Vel donec et scelerisque vestibulum. Condimentum aliquam, mollit magna velit nec, tempor cursus vitae sit aliquet neque purus. Ultrices lacus proin conubia dictum tempus, tempor pede vitae faucibus, sem auctor, molestie diam dictum aliquam. Dolor leo, ridiculus est ut cubilia nec, fermentum arcu praesent, pede etiam. Tempor vestibulum turpis id ligula mi mattis. Eget arcu vitae mauris amet odio.
+						Diam nibh diam, quam elit, libero nostra ut. Pellentesque vehicula.</p>
 				</article>
 				<ul id="social_media">
 					<li>
